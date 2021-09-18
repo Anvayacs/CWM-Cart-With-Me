cebbc7733671c6c2c689a4f4c5ed85de8a0e0eeb
Anvay Arora <75296875+the-protagonist-view@users.noreply.github.com>
2021-09-18 14:58:41 +0530

    Add files via upload

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -62,7 +62,7 @@ export default function ProfileScreen() {
     <div>
       <form className="form" onSubmit={submitHandler}>
         <div>
-          <h1>User Profile</h1>
+          <h1>Profile</h1>
         </div>
         {loading ? (
           <LoadingBox></LoadingBox>
